c67a14dea6d092019ab75eea69f075f74d47033b
Francisco Jose Torres Sandoval <ftorressa@ucenfotec.ac.cr>
2025-11-17 21:47:22 -0600

    Entregable Practica 2  Francisco Torres

---
+++ mi-archivo.js
@@ -0,0 +1,13 @@
+console.log("¡Hola, mundo desde JavaScript!");
+
+function saludar(nombre) {
+    console.log("Hola, " + nombre + ". ¡Bienvenida al ejercicio de Git y GitHub!");
+}
+
+function sumar(a, b) {
+    let resultado = a + b;
+    console.log("La suma de " + a + " + " + b + " es: " + resultado);
+}
+
+saludar("nombre");
+sumar(5, 3);
